2f87679b8bc96ee97e1e0d5ee44d66f079dcc0de
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-20 23:38:27 +1000

    Navbar links and design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import logo from '../logo.svg'
+
 
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h3>hello from navbar</h3>
-            </div>
+            <nav className='navbar navbar-expand-sm bg-primary navbar-dark px-sm-5'>
+                <Link to='/'>
+                    <img src={logo} alt='logo' className='navbar-brand'></img>
+                </Link>
+                <ul className='navbar-nav align-items-center'>
+                    <li className='nav-item ml-5'>
+                        <Link to='/' className='nav-link'>
+                            Products
+                        </Link>
+                    </li>
+                </ul>
+                <Link to='cart' className='ml-auto'>
+                    <button>
+                        <i class="shopping cart plus icon"></i>
+                        My Cart
+                    </button>
+                </Link>
+            </nav>
         )
     }
 }
