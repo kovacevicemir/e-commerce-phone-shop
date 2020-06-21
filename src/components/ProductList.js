fe5d1e384bd0c50249a0aa9cc28c08ac2bcd9800
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-21 16:10:19 +1000

    Using Context

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Product from './Product'
 import Title from './Title'
 import {storeProducts} from '../data'
+import {ProductConsumer} from '../context'
 
 export default class ProductList extends Component {
     state={
@@ -16,7 +17,11 @@ export default class ProductList extends Component {
                     <div className='container'>
                         <Title name='our' title='products'/>
                         <div className='row'>
-
+                            <ProductConsumer>
+                                {(value)=>{
+                                    return <h1>{value}</h1>
+                                }}
+                            </ProductConsumer>
                         </div>
                     </div>
                 </div>
