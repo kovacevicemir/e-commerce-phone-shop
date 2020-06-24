b9e7316ef9139b8343608119731b8de00bfa5fef
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-25 01:19:00 +1000

    modal design and implementation done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ProductList from './components/ProductList'
 import Details from './components/Details'
 import Cart from './components/Cart'
 import Default from './components/Default'
+import Modal from './components/Modal'
 
 
 class App extends Component {
@@ -21,6 +22,7 @@ class App extends Component {
           <Route exact path='/cart' component={Cart} />
           <Route component={Default} />
         </Switch>
+        <Modal/>
       </React.Fragment>
     )
   }
